720e71e756ec36b643d6e8828adcbfa82e394b9f
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-04-24 20:28:16 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './js/tictac';
+fetch('https://api.consumet.org/anime/gogoanime/demon?page=2')
+.then(r => r.json())
+.then(d => console.log(d))
+.catch(e => console.log(e))
+
+fetch('https://api.consumet.org/anime/gogoanime/watch/spy-x-family-episode-1')
+.then(r => r.json())
+.then(d => console.log(d))
+.catch(e => console.log(e))
+console.log(`wow`);
