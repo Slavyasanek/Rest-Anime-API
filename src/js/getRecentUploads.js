fc77299d4e76f58b1611e5fe9c8e0f33f54925fc
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-04-27 18:48:06 +0300

    update function

--- src/js/getRecentUploads.js
+++ src/js/getRecentUploads.js
@@ -1,30 +1,32 @@
 import { getRecentUploads } from "./functions/api";
 import { refs } from './refs';
 import { createPaginationForRecentRealese } from "./functions/pagination";
-import {renderCard} from './functions/renderCard';
+import { renderCard } from './functions/renderCard';
 import { finishLoad, startLoading } from "./functions/loading";
+import { setCurrentLink, setDatasetAnimeList } from "./functions/changeCurrentLink";
 
 
 
-const getAnime = () => {
-    getRecentUploads(1)
-        .then(({ results }) => {
+const getAnime = async () => {
+    setDatasetAnimeList('recent');
+    setCurrentLink();
+    startLoading();
+    const { results } = await getRecentUploads(1);
+    const posts = renderCard(results);
+    refs.pagination.innerHTML = "";
+    refs.animeList.innerHTML = posts;
+    finishLoad();
+
+    const pagination = createPaginationForRecentRealese(results.length);
+    pagination.on('beforeMove', ({ page }) => {
+        refs.animeList.innerHTML = "";
+        startLoading();
+        getRecentUploads(page).then(({ results }) => {
             const posts = renderCard(results);
-            refs.animeList.insertAdjacentHTML("beforeend", posts)
-            const pagination = createPaginationForRecentRealese(results.length);
-            pagination.on('beforeMove', ({ page }) => {
-                refs.animeList.innerHTML = "";
-                startLoading()
-                getRecentUploads(page).then(({ results }) => {
-                    const posts = renderCard(results);
-                    refs.animeList.insertAdjacentHTML("beforeend", posts);
-                    finishLoad()
-                })
-            })
-        }).catch(e => {
-            console.log(e);
+            refs.animeList.insertAdjacentHTML("beforeend", posts);
+            finishLoad();
         })
-
+    })
 }
 
 
