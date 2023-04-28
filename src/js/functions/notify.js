9778b54337cf95a6f9abc8d650d0c124b6462a19
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-04-28 17:57:58 +0300

    add notify no storage info

--- src/js/functions/notify.js
+++ src/js/functions/notify.js
@@ -15,3 +15,15 @@ export const infoNoResults = () => {
         fontSize: '18px',
     })
 }
+
+export const messageNoAnimeInStorage = (storageType) => {
+    return `<div class="library__message">
+    <h2 class="library__text">No ${storageType} anime yet</h2>
+    <img
+      src="https://cdn.vox-cdn.com/thumbor/t5tvuSLje1qJDX4dr4-mXK_00eY=/0x0:1920x1080/1200x800/filters:focal(807x387:1113x693)/cdn.vox-cdn.com/uploads/chorus_image/image/70922041/Screen_Shot_2022_05_26_at_10.49.28_AM.0.png"
+      alt="anya"
+      width="200"
+      class="library__img"
+    />
+</div>`
+}
